97ad5599130ebedbebdbcc85589361a5982b37e0
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2020-10-09 11:23:07 +0300

    Updates
    * Admin ismi değiştirilldi
    * 404 sayfa
    - Fazlalık sayfalar silindi

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -40,9 +40,9 @@ const NotFoundPage: React.FC<NotFoundTemplateProps> = props => {
           <div css={[inner]}>
             <section style={{ textAlign: 'center' }}>
               <ErrorCode>404</ErrorCode>
-              <ErrorDescription>Page not found</ErrorDescription>
+              <ErrorDescription>Sayfa Bulunamadı</ErrorDescription>
               <Link css={ErrorLink} to="">
-                Go to the front page →
+                Ana Sayfaya Dön →
               </Link>
             </section>
 
