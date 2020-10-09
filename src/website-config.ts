495af085daa22f1999789b5fdaddb2729383ef3a
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2020-10-09 11:02:16 +0300

    Update Website Config

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,14 +49,14 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
+  title: 'Undefined Objects',
   description: 'Undefined Objects',
   coverImage: 'img/blog-cover.png',
   logo: 'img/ghost-logo.png',
-  lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  lang: 'tr',
+  siteUrl: 'https://undefinedobjects.github.io',
+  facebook: 'https://www.facebook.com/undefinedobjects',
+  twitter: 'https://twitter.com/undefinedobjects',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
